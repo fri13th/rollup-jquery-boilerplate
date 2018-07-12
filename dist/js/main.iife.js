0d9a910f15fba34a410473109f9f844cdc60606c
fri13th <tenderain@gmail.com>
2018-07-12 17:10:44 +0900

    generate two different version

---
+++ dist/js/main.iife.js
@@ -0,0 +1,2 @@
+!function(){"use strict";var n,o=[1,2,3];console.log((n=3)*n*n),$(function(){return console.log("\nHello, "+"World"+"!\n"+o.map(function(n){return""+n}).join("")+"\n")})}();
+//# sourceMappingURL=main.iife.js.map
--- rollup.config.js
+++ rollup.config.js
@@ -3,29 +3,55 @@ import babelrc from 'babelrc-rollup';
 import postcss from 'rollup-plugin-postcss';
 import resolve from 'rollup-plugin-node-resolve';
 import browsersync from 'rollup-plugin-browsersync';
+import { uglify } from 'rollup-plugin-uglify';
 import autoprefixer from 'autoprefixer';
+
+const production = !process.env.ROLLUP_WATCH;
+let plugins = production
+  ? [
+      resolve(),
+      postcss({
+        extract: './dist/css/main.css',
+        minimize: true,
+        sourceMap: true,
+        plugins: [
+          autoprefixer({
+            grid: true
+          })
+        ]
+      }),
+      babel(babelrc()),
+      uglify()
+    ]
+  : [
+      resolve(),
+      browsersync({ server: 'dist' }),
+      postcss({
+        extract: './dist/css/main.css',
+        plugins: [
+          autoprefixer({
+            grid: true
+          })
+        ]
+      }),
+      babel(babelrc())
+    ];
 
 export default {
   input: 'src/js/main.js',
-  output: {
-    file: 'dist/js/main.min.js',
-    format: 'iife',
-    globals: { jquery: '$' }
-  },
-  plugins: [
-    resolve(),
-    browsersync({ server: 'dist' }),
-    // https://github.com/egoist/rollup-plugin-postcss
-    postcss({
-      extract: './dist/css/main.min.css',
-      minimize: true,
-      sourceMap: true,
-      plugins: [
-        autoprefixer({
-          grid: true
-        })
-      ]
-    }),
-    babel(babelrc())
-  ]
+  output: [
+    {
+      file: 'dist/js/main.iife.js',
+      format: 'iife',
+      globals: { jquery: '$' },
+      sourcemap: true
+    },
+    {
+      file: 'dist/js/main.es6.js',
+      format: 'es',
+      globals: { jquery: '$' },
+      sourcemap: true
+    }
+  ],
+  plugins
 };
